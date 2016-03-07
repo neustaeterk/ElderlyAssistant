e35cf4c6c97ca6b5c4396133396f2935825d4b96
Shahar <sr.concordia@gmail.com>
2016-03-07 18:02:08 -0500

    this is a test

--- app/src/main/java/com/example/kenton/elderlyassistant/MainActivity.java
+++ app/src/main/java/com/example/kenton/elderlyassistant/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // hey changes
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
