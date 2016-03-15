477d19d683e715f9ffd8de615772884e4a41b165
Kenton <neustaeterk@gmail.com>
2016-03-14 23:21:01 -0400

    update schedule layout add some stuff to try to change the background colours in the medications list (nothing working)

--- app/src/main/java/com/example/kenton/elderlyassistant/MedicationsListFragment.java
+++ app/src/main/java/com/example/kenton/elderlyassistant/MedicationsListFragment.java
@@ -1,5 +1,6 @@
 package com.example.kenton.elderlyassistant;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,21 +36,43 @@ public class MedicationsListFragment extends Fragment {
         mMedList = mDb.getAllReminders();
 
         ArrayList<String> medicationsNames = new ArrayList<String>();
+        ArrayList medicationDismiss = new ArrayList();
 
         for (int i = 0; i < mMedList.size();i++){
             medicationsNames.add(mMedList.get(i).getMedicationName());
             //Log.d("XXXXXXX", medicationsNames.get(i));
+            medicationDismiss.add(mMedList.get(i).getDismissed());
         }
-
-
-        mMedListAdapter = new ArrayAdapter<String>
-                (getActivity(), R.layout.list_medications, R.id.list_medications_textview, medicationsNames);
 
         View rootView = inflater.inflate(R.layout.fragment_medications_list, container, false);
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_medications);
+
+        mMedListAdapter = new ArrayAdapter<String>
+                (getActivity(), R.layout.list_medications, R.id.list_medications_textview, medicationsNames);
+
         listView.setAdapter(mMedListAdapter);
 
+        //View view = getViewByPosition(0, listView);
+        //view.setBackgroundColor(Color.parseColor("red"));
+
+        //int count = mMedListAdapter.getCount();
+        //int pos = 0;
+        //View view = mMedListAdapter.getView(pos, (TextView)rootView.findViewById(R.id.list_medications_textview), listView);
+        //view.setBackgroundColor(Color.parseColor("red"));
+
         return rootView;
+    }
+
+    public View getViewByPosition(int pos, ListView listView) {
+        final int firstListItemPosition = listView.getFirstVisiblePosition();
+        final int lastListItemPosition = firstListItemPosition + listView.getChildCount() - 1;
+
+        if (pos < firstListItemPosition || pos > lastListItemPosition ) {
+            return listView.getAdapter().getView(pos, null, listView);
+        } else {
+            final int childIndex = pos - firstListItemPosition;
+            return listView.getChildAt(childIndex);
+        }
     }
 }
